b66ab3b277bfa9c3b627d2fac685c68df0e4e1fc
biel.lil <78098857+biellil@users.noreply.github.com>
2023-02-05 21:05:44 -0300

    05/02

--- code.js
+++ code.js
@@ -1,6 +1,14 @@
 const button = document.getElementById('menu-icon');
 const sidebar = document.querySelector('.transition');
+const dashboard = document.querySelector(".tela");
 
 button.addEventListener('click', () => {
+  
   sidebar.classList.toggle('active');
+  tela.classList.toggle("active");
 });
+
+
+
+
+
