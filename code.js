c82bc21acd7a26d9c94d495db342ad2a6e9ee47a
biel.lil <78098857+biellil@users.noreply.github.com>
2023-02-07 16:43:28 -0300

    07/02

--- code.js
+++ code.js
@@ -1,12 +1,18 @@
 const button = document.getElementById('menu-icon');
 const sidebar = document.querySelector('.transition');
 const dashboard = document.querySelector(".tela");
-const outBar =  document.querySelector9('.outBar')
+const outtBar =  document.querySelector('.outt')
+const outBar =  document.querySelector('.out')
+
+
 
 button.addEventListener('click', () => {
-  
   sidebar.classList.toggle('active');
   dashboard.classList.toggle('active');
+  outtBar.classList.toggle('active')
   outBar.classList.toggle('active')
-
+ 
 });
+
+
+
